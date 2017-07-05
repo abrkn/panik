75c4df69d549ab7914a2f339dcdef4b29d363491
Andreas <andreas@MacBook-Pro.local>
2017-07-05 09:42:57 +0200

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+function printErrorAndExit(error) {
+  console.error(error.stack || error || 'Unknown error');
+  process.exit(error.code || 1);
+}
+
+process.on('unhandledRejection', function(error) {
+  setImmediate(function() {
+    printErrorAndExit(error);
+  });
+});
