a37843f11915ea705c5982118f13e531cff3d081
Andreas Brekken <andreas@brekken.com>
2018-11-26 12:58:27 +0700

    Add debugging

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const debug = require('debug')('panik');
+
 const printErrorAndExit = error => {
+  const code = error.code || 1;
+
+  debug(`Printing error and exiting with code ${code}...`);
+
   console.error(error.stack || error || 'Unknown error');
-  process.exit(error.code || 1);
+  process.exit(code);
 };
 
 process.on('unhandledRejection', function(error) {
+  debug(`Unhandled rejection in process. Printing error and exiting`);
+
   setImmediate(function() {
     printErrorAndExit(error);
   });
 });
+
+debug('Attached unhandledRejection handler');
 
 Object.assign(exports, { printErrorAndExit });
