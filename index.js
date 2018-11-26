31b721371b342e92942e709dc944e1d62e0dc234
Andreas Brekken <andreas@brekken.com>
2018-11-26 11:46:56 +0700

    Formatting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const printErrorAndExit = error => {
-    console.error(error.stack || error || 'Unknown error');
-    process.exit(error.code || 1);
+  console.error(error.stack || error || 'Unknown error');
+  process.exit(error.code || 1);
 };
 
 process.on('unhandledRejection', function(error) {
-    setImmediate(function() {
-        printErrorAndExit(error);
-    });
+  setImmediate(function() {
+    printErrorAndExit(error);
+  });
 });
 
 Object.assign(exports, { printErrorAndExit });
